a082ac343ffa49c2789735626f43d951e866eedb
anton.korniichuk <anton.korniichuk@xenial.com>
2020-02-06 09:00:34 +0200

    3-1 2

--- src/3-1-typeahead/main.spec.ts
+++ src/3-1-typeahead/main.spec.ts
@@ -13,7 +13,7 @@ describe('Marble testing in RxJS', () => {
     });
   });
 
-  it('', () => {
+  it('Should debounce input by 200 ms', () => {
     testScheduler.run(({ cold, expectObservable }) => {
       const searchTerm = 'testing';
       const breweries = [{ name: 'Brewery 1' }];
@@ -26,6 +26,27 @@ describe('Marble testing in RxJS', () => {
       );
       const expected = '500ms a';
       const expectedValues = { a: ['Brewery 1'] };
+
+      expectObservable(final$).toBe(expected, expectedValues);
+    });
+  });
+
+  it('Should cancel active requests if another value is emitted', () => {
+    testScheduler.run(({ cold, expectObservable }) => {
+      const searchTerm = 'testing';
+      const breweries = [{ name: 'Brewery 1' }];
+
+      const source$ = cold('a 250ms b', {
+        a: { target: { value: 'first' } },
+        b: { target: { value: 'second' } },
+      });
+      const final$ = source$.pipe(
+        breweryTypeahead({
+          getJSON: () => $$.of(breweries).pipe($.delay(300)),
+        } as any),
+      );
+      const expected = '- 750ms c';
+      const expectedValues = { c: ['Brewery 1'] };
 
       expectObservable(final$).toBe(expected, expectedValues);
     });
